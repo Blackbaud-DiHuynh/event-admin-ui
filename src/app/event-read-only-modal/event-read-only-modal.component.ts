3f256fdf70a74f5cdb6982e352f24d7723e63776
Blackbaud-AliRashed <ali.rashed@blackbaud.com>
2017-07-27 22:07:44 -0500

    read only modal

--- src/app/event-read-only-modal/event-read-only-modal.component.ts
+++ src/app/event-read-only-modal/event-read-only-modal.component.ts
@@ -3,6 +3,8 @@ import { SkyModalInstance } from '@blackbaud/skyux/dist/modules/modal';
 import { Event } from '../shared/Event';
 import { Operation } from '../shared/operation';
 import { EventReadOnlyModalContext } from './event-read-only-modal-context';
+import { DynamicRule } from '../shared/DynamicRule';
+import { DynamicRuleService } from '../shared/DynamicRuleService';
 
 @Component({
     selector: 'event-read-only-modal',
@@ -10,15 +12,22 @@ import { EventReadOnlyModalContext } from './event-read-only-modal-context';
 })
 export class EventReadOnlyModalComponent implements OnInit {
     public event: Event;
+    public dynamicRules: DynamicRule[] = [new DynamicRule];
     public operation: Operation;
     public format12 = 'hh';
     public price: number;
     public selectedTime;
 
     constructor(public modalInstance: SkyModalInstance,
-                public context: EventReadOnlyModalContext){ }
+                public context: EventReadOnlyModalContext,
+                private service: DynamicRuleService){ }
 
     public ngOnInit(): void {
-       console.log(this.context.event);
+        this.event = this.context.event;
+        this.service.getDynamicRules(this.event.tickets[0]).subscribe(
+            returnedRules => {
+                this.dynamicRules = returnedRules;
+            }
+        );
     }
 }
--- src/app/shared/DynamicRuleService.ts
+++ src/app/shared/DynamicRuleService.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response, Headers } from '@angular/http';
 import { Observable } from 'rxjs';
 import { DynamicRule } from './DynamicRule';
+import { Ticket } from './Ticket';
 
 @Injectable()
 export class DynamicRuleService {
@@ -13,6 +14,17 @@ export class DynamicRuleService {
         let headers = new Headers({'Accept': 'application/json'});
         return this.http
             .post(this.serviceUrl, dynamicRule, { headers })
+            .map((res: Response) => res.json())
+            .catch((error) => {
+                return Observable.throw(error);
+            });
+    }
+
+    public getDynamicRules(ticket: Ticket): Observable<DynamicRule[]> {
+        let headers = new Headers({'Accept': 'application/json'});
+        let ticketUrl = `${this.serviceUrl}/${ticket.id}`;
+        return this.http
+            .get(ticketUrl, { headers })
             .map((res: Response) => res.json())
             .catch((error) => {
                 return Observable.throw(error);
